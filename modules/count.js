64ef696a8c288d3367f9ca8a28bc227e2c6e2232
Shooty318 <gp22a112@oecu.jp>
2023-10-23 18:59:15 +0900

    countModule作りました。

---
+++ modules/count.js
@@ -0,0 +1,47 @@
+const { 
+    ActionRowBuilder,
+    SlashCommandBuilder,
+    TextInputStyle,
+    ModalBuilder,
+    TextInputBuilder,
+    EmbedBuilder,
+} = require('discord.js')
+
+const config = require('../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('文字数カウント')
+        .setDescription('2000文字までカウントできます。'),
+    async execute(interaction){
+        const modal = new ModalBuilder()
+        .setCustomId('count')  
+        .setTitle('文字数カウント(2000文字までカウントできます)');
+        
+        const text = new TextInputBuilder()
+            .setCustomId('text')
+            .setLabel("本文")
+            .setStyle(TextInputStyle.Short);
+                
+        modal.addComponents(
+            new ActionRowBuilder().addComponents(text),    
+        );
+        
+        await interaction.showModal(modal);
+        const filter = (mInteraction) => mInteraction.customId === 'count';
+        interaction.awaitModalSubmit({ filter, time: 600000 })
+            .then(async mInteraction => {
+                const text = mInteraction.fields.getTextInputValue('text');
+                const emtext = text.slice(0,100) + "...";
+                return mInteraction.reply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle(`文字数：${text.length}\n\n本文：${emtext}`)
+                            .setColor(7506394)
+                    ]
+                })
+
+            })
+            .catch(console.error);
+    }
+}
