bc8ec60ae3e7bd7238403079c2e290750d966fd3
harusann <gp22a074@oecu.jp>
2023-10-24 04:58:47 +0900

    再度リファクタ

--- command/submit_commands.js
+++ command/submit_commands.js
@@ -1,19 +1,21 @@
 // discord.js v14では、下記のようにRESTとRoutesはdiscord.jsパッケージから直接インポートできます
 const { REST, Routes } = require('discord.js');
 
-// hey.jsのmodule.exportsを呼び出します。
-const nouritu = require('../modules/information_theory.js');
-const trans = require('../modules/translation.js');
-const count = require('../modules/count.js');
-const logDeletion = require('./log-deletion.js');
-const logDeletionAll = require('./log-deletion-all.js');
+
+// 登録コマンドを呼び出してリスト形式で登録
+const commands = [
+    require('../modules/information_theory.js').data.toJSON(), 
+    require('../modules/translation.js').data.toJSON(), 
+    require('../modules/count.js').data.toJSON(), 
+    require('./log-deletion.js').data.toJSON(), 
+    require('./log-deletion-all.js').data.toJSON()
+];
+
 
 
 // 環境変数としてapplicationId, guildId, tokenの3つが必要です
 const { prkn_token, prkn_applicationId, prkn_guildId } = require('../config.json');
 
-// 登録コマンドを呼び出してリスト形式で登録
-const commands = [nouritu.data.toJSON(), trans.data.toJSON(), count.data.toJSON(), logDeletion.data.toJSON(), logDeletionAll.data.toJSON()];
 
 // DiscordのAPIには現在最新のversion10を指定
 const rest = new REST({ version: '10' }).setToken(prkn_token);
